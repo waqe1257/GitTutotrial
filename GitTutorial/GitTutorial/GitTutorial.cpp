a1316a6bc9ebe1194a81b0e043f9264d37c5bc5b
YuHeng Wang <mssu1900@gmail.com>
2017-02-24 11:58:40 +0800

    GitTutorial 2

---
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace System;
+bool Guess(int number) {
+	return false;
+}
+int main(array<System::String ^> ^args)
+{
+	return 0;
+}
