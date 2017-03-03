584a86c8920e1e3de582876089ea64a00f43e66f
Devine Tu <waqe1257@gmail.com>
2017-03-03 09:50:00 +0800

    no message

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -9,7 +9,15 @@ bool Guess(int number) {
 		target = r.Next() % 100 + 1;
 	}
 	if (number == target)return true;
-	return false;
+	if (number > target) {
+		std::cout << "Smaller" << std::endl;
+		return false;
+	}
+	else if (number < target) {
+		std::cout << "Bigger" << std::endl;
+		return false;
+	}
+	return true;
 }
 int main(array<System::String ^> ^args)
 {
