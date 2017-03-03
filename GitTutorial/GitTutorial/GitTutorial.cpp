aa50c5405078962aabaf8f113356f27fad7bd143
1-3\NTUTCSIE <mssu1900@gmail.com>
2017-03-03 09:58:00 +0800

    no message

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -8,22 +8,18 @@ bool Guess(int number) {
 		Random r;
 		target = r.Next() % 100 + 1;
 	}
-	if (number == target)return true;
-	if (number > target) {
-		std::cout << "Smaller" << std::endl;
-		return false;
+	if (number == target) {
+		std::cout << "Correct !!";
+		target = -1;
+		return true;
 	}
-	else if (number < target) {
-		std::cout << "Bigger" << std::endl;
-		return false;
-	}
-	return true;
+	else std::cout << "Wrong" << std::endl;
+	return false;
 }
 int main(array<System::String ^> ^args)
 {
 	int guess;
 	do {
-		std::cout << "Choose a number between 1 - 100 :";
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;
